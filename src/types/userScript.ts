c4246e4a2289a17ad46507e6cfe165b7620f7778
涤尘 <dichen.lzy@alibaba-inc.com>
2022-03-09 16:43:56 +0800

    chore: 增加postJson

--- src/types/userScript.ts
+++ src/types/userScript.ts
@@ -5,7 +5,8 @@ import { ApiResponse, OverviewApiResponse, ProjectConfig, ProjectResponse } from
  * 可通过fetchJson，拉取服务端json数据，不受同源限制，拥有高权限
  */
 export interface Context {
-  fetchJSON: <Response>(...args: Parameters<typeof fetch>) => Promise<Response>;
+  fetchJSON: <Response = any>(...args: Parameters<typeof fetch>) => Promise<Response>;
+  postJSON: <Response = any>(...args: Parameters<typeof fetch>) => Promise<Response>;
 }
 
 /**
@@ -21,10 +22,10 @@ export type GetApiRequest = (project: ProjectConfig, api: OverviewApiResponse, c
 /**
  * 更改api场景数据，由开发者自定义
  */
-export type UpdateApiSceneRequest<Response> = (project: ProjectConfig, api: ApiResponse, context: Context) => Promise<Response>;
+export type UpdateApiSceneRequest<Response = any> = (project: ProjectConfig, api: ApiResponse, context: Context) => Promise<Response>;
 
 export interface UserScript {
   getProject: GetProjectRequest;
   getApi: GetApiRequest;
-  updateApiScene?: UpdateApiSceneRequest<any>;
+  updateApiScene?: UpdateApiSceneRequest;
 }
